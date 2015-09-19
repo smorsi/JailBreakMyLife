406d9ec87e81110f0871f1ee48f1d69806c9c101
Sara Morsi <smorsi27@gmail.com>
2015-09-19 16:48:50 -0400

    initial code for project

---
+++ JailBreaker/JailBreaker/Controllers/EducationController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JailBreaker.Controllers
+{
+    public class EducationController : Controller
+    {
+        // GET: Education
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Math()
+        {
+            return View();
+        }
+
+        public ActionResult Science()
+        {
+            return View();
+        }
+
+        public ActionResult Technology()
+        {
+            return View();
+        }
+    }
+}
